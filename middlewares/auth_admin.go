001233fac1177f74a2343a51571f2f3bd5d16599
songxy <songxychina@gmail.com>
2022-10-29 10:52:33 +0800

    拦截器放行OPTIONS请求

--- middlewares/auth_admin.go
+++ middlewares/auth_admin.go
@@ -10,11 +10,16 @@ import (
 
 func AuthAdminCheck() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// 放行OPTIONS请求
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+
 		auth := c.GetHeader("token")
 		if auth == "" {
 			c.JSON(http.StatusOK, gin.H{
 				"code": -1,
-				"msg":"无登录凭证，请先登录",
+				"msg":  "无登录凭证，请先登录",
 			})
 			c.Abort()
 			return
@@ -24,14 +29,14 @@ func AuthAdminCheck() gin.HandlerFunc {
 			if err == jwt.ErrTokenExpired {
 				c.JSON(http.StatusOK, gin.H{
 					"code": -1,
-					"msg":"登录凭证已过期，请重新登录",
+					"msg":  "登录凭证已过期，请重新登录",
 				})
 				c.Abort()
 				return
 			}
 			c.JSON(http.StatusOK, gin.H{
 				"code": -1,
-				"msg":"错误的登录凭证，请重新登录",
+				"msg":  "错误的登录凭证，请重新登录",
 			})
 			c.Abort()
 			return
--- middlewares/cors.go
+++ middlewares/cors.go
@@ -8,7 +8,6 @@ import (
 
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		method := c.Request.Method
 		origin := c.Request.Header.Get("Origin")
 		if origin != "" {
 			c.Header("Access-Control-Allow-Origin", "*") // 可将将 * 替换为指定的域名
@@ -17,7 +16,7 @@ func Cors() gin.HandlerFunc {
 			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
 			c.Header("Access-Control-Allow-Credentials", "true")
 		}
-		if method == "OPTIONS" {
+		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
 		}
 		c.Next()
